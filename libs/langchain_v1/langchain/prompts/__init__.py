478284ff1fcab6f16680366217e78d4ca21ae132
Eugene Yurtsev <eyurtsev@gmail.com>
2025-07-21 22:20:41 -0400

    x

--- libs/langchain_v1/langchain/prompts/__init__.py
+++ libs/langchain_v1/langchain/prompts/__init__.py
@@ -28,8 +28,6 @@ from multiple components. Prompt classes and functions make constructing
 
 """  # noqa: E501
 
-from typing import TYPE_CHECKING, Any
-
 from langchain_core.example_selectors import (
     LengthBasedExampleSelector,
     MaxMarginalRelevanceExampleSelector,
@@ -46,36 +44,10 @@ from langchain_core.prompts import (
     FewShotPromptWithTemplates,
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
-    PipelinePromptTemplate,
     PromptTemplate,
     StringPromptTemplate,
     SystemMessagePromptTemplate,
-    load_prompt,
 )
-
-from langchain._api import create_importer
-from langchain.prompts.prompt import Prompt
-
-if TYPE_CHECKING:
-    from langchain_community.example_selectors.ngram_overlap import (
-        NGramOverlapExampleSelector,
-    )
-
-# Create a way to dynamically look up deprecated imports.
-# Used to consolidate logic for raising deprecation warnings and
-# handling optional imports.
-MODULE_LOOKUP = {
-    "NGramOverlapExampleSelector": (
-        "langchain_community.example_selectors.ngram_overlap"
-    ),
-}
-
-_import_attribute = create_importer(__file__, module_lookup=MODULE_LOOKUP)
-
-
-def __getattr__(name: str) -> Any:
-    """Look up attributes dynamically."""
-    return _import_attribute(name)
 
 
 __all__ = [
@@ -91,12 +63,8 @@ __all__ = [
     "LengthBasedExampleSelector",
     "MaxMarginalRelevanceExampleSelector",
     "MessagesPlaceholder",
-    "NGramOverlapExampleSelector",
-    "PipelinePromptTemplate",
-    "Prompt",
     "PromptTemplate",
     "SemanticSimilarityExampleSelector",
     "StringPromptTemplate",
     "SystemMessagePromptTemplate",
-    "load_prompt",
 ]
